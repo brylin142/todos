587524f00eec30af0836de59a217362adeba0f11
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-09-06 14:52:49 -0700

    Create actions and reducer

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Todos App</h1>, root);
-})
+});
