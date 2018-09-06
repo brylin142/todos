91bee379fb51397b766f8b58cfbfb329559110f9
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-09-06 16:50:39 -0700

    Add actions to reducer

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,6 +1,18 @@
+import { RECEIVE_TODOS, RECEIVE_TODO } from '../actions/todo_actions';
+import merge from 'lodash/merge'
+
 const todosReducer = (state = {}, action) => {
   Object.freeze(state);
+  let nextState = {};
+
   switch(action.type) {
+    case RECEIVE_TODOS:
+      action.todos.forEach(todo => {
+        nextState[action.id] = todo;
+      });
+      return nextState;
+    case RECEIVE_TODO:
+      return merge({}, state, { [action.todo.id]: action.todo });
     default:
       return state;
   }
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = {};
+  const store = configureStore(preloadedState);
+
+  window.store = store;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Todos App</h1>, root);
 });
